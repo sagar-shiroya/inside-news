ee08661e984073ee2ef99ac7dc637452e8f5e33b
Sagar Shiroya <sagar.shiroya8@gmail.com>
2021-12-12 15:17:07 +0530

    Added cards component for bootstrap, passed props to NewsItem

--- src/components/News.js
+++ src/components/News.js
@@ -4,12 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                This is news component.
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
+            <div className="container my-3">
+                <h2>InsideNews - Top Headlines</h2>
+                <div className='row my-4'>
+                    <div className="col md-4">
+                        <NewsItem title="myTitle" description="myDescription"/>
+                    </div>
+                    <div className="col md-4">
+                        <NewsItem title="myTitle" description="myDescription"/>
+                    </div>
+                    <div className="col md-4">
+                        <NewsItem title="myTitle" description="myDescription"/>
+                    </div>
+                </div>
             </div>
         )
     }
